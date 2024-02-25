af8f9b1753fb4f7727bdb4ee064c6af6f4b7f2fe
aminedevaj <developerwebaj@gmail.com>
2024-02-25 12:53:18 +0100

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import logo from './favicon.ico';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+       
+  
+        <h1 class="text-accent">
+    Hello world!
+  </h1>
+   
+      
+      </header>
+    </div>
+  );
+}
+
+export default App;
